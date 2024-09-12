131efddbe7cafdeb14d72b61bfe161ec1034c830
Nykau16 <Nykau16@gmail.com>
2024-09-12 12:41:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+function ManipulaConteudo1()
+{
+    var info1 = document.getElementById("info1");
+    var info2 = document.getElementById("info2");
+    var info3 = document.getElementById("info3");
+    var info4 = document.getElementById("info4");
+    var info5 = document.getElementById("info5");
+    var info6 = document.getElementById("info6");
+
+    if(document.getElementById("option1").onclick)
+    {
+        info1.style.display = "flex";
+        info2.style.display = "none";
+        info3.style.display = "none";
+        info4.style.display = "none";
+        info5.style.display = "none";
+        info6.style.display = "none";
+    }
+}
+
+function ManipulaConteudo2()
+{
+    var info1 = document.getElementById("info1");
+    var info2 = document.getElementById("info2");
+    var info3 = document.getElementById("info3");
+    var info4 = document.getElementById("info4");
+    var info5 = document.getElementById("info5");
+    var info6 = document.getElementById("info6");
+
+    if(document.getElementById("option1").onclick)
+    {
+        info1.style.display = "none";
+        info2.style.display = "flex";
+        info3.style.display = "none";
+        info4.style.display = "none";
+        info5.style.display = "none";
+        info6.style.display = "none";
+    }
+}
+
+
+function ManipulaConteudo3()
+{
+    var info1 = document.getElementById("info1");
+    var info2 = document.getElementById("info2");
+    var info3 = document.getElementById("info3");
+    var info4 = document.getElementById("info4");
+    var info5 = document.getElementById("info5");
+    var info6 = document.getElementById("info6");
+
+    if(document.getElementById("option1").onclick)
+    {
+        info1.style.display = "none";
+        info2.style.display = "none";
+        info3.style.display = "flex";
+        info4.style.display = "none";
+        info5.style.display = "none";
+        info6.style.display = "none";
+    }
+}
+
+
+function ManipulaConteudo4()
+{
+    var info1 = document.getElementById("info1");
+    var info2 = document.getElementById("info2");
+    var info3 = document.getElementById("info3");
+    var info4 = document.getElementById("info4");
+    var info5 = document.getElementById("info5");
+    var info6 = document.getElementById("info6");
+
+    if(document.getElementById("option1").onclick)
+    {
+        info1.style.display = "none";
+        info2.style.display = "none";
+        info3.style.display = "none";
+        info4.style.display = "flex";
+        info5.style.display = "none";
+        info6.style.display = "none";
+    }
+}
+
+
+function ManipulaConteudo5()
+{
+    var info1 = document.getElementById("info1");
+    var info2 = document.getElementById("info2");
+    var info3 = document.getElementById("info3");
+    var info4 = document.getElementById("info4");
+    var info5 = document.getElementById("info5");
+    var info6 = document.getElementById("info6");
+
+    if(document.getElementById("option1").onclick)
+    {
+        info1.style.display = "none";
+        info2.style.display = "none";
+        info3.style.display = "none";
+        info4.style.display = "none";
+        info5.style.display = "flex";
+        info6.style.display = "none";
+    }
+}
+
+function ManipulaConteudo6()
+{
+    var info1 = document.getElementById("info1");
+    var info2 = document.getElementById("info2");
+    var info3 = document.getElementById("info3");
+    var info4 = document.getElementById("info4");
+    var info5 = document.getElementById("info5");
+    var info6 = document.getElementById("info6");
+
+    if(document.getElementById("option1").onclick)
+    {
+        info1.style.display = "none";
+        info2.style.display = "none";
+        info3.style.display = "none";
+        info4.style.display = "none";
+        info5.style.display = "none";
+        info6.style.display = "flex";
+    }
+}
